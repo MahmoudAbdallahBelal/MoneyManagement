7fb8dff481bb26311606f75c7f15e7dd5b7a17bf
MahmoudBelal <mahmoudbelal955@gmail.com>
2018-02-25 15:39:01 +0200

    Initial commit

---
+++ app/src/main/java/mab/moneymanagement/view/SplashActivity.java
@@ -0,0 +1,14 @@
+package mab.moneymanagement.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import mab.moneymanagement.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
