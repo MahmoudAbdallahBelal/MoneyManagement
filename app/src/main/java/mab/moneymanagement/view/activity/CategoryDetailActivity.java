0e12c29868893b839ee56dd7c1c417366c39b1c5
Gihan <gemy6987@gmail.com>
2018-03-04 06:10:08 -0800

    My commit message

---
+++ app/src/main/java/mab/moneymanagement/view/activity/CategoryDetailActivity.java
@@ -0,0 +1,31 @@
+package mab.moneymanagement.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import mab.moneymanagement.R;
+import mab.moneymanagement.view.fragment.ChartFragment;
+
+public class CategoryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_detail);
+
+        //TO PUT FRAGMENT ON ACTIVITY
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.category_detail_ativity, new ChartFragment())
+                    .commit();
+        }
+
+
+        Toolbar mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.category_detail_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(getString(R.string.category_detail_title));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
---
+++ app/src/main/java/mab/moneymanagement/view/fragment/CategoryDetailFragment.java
@@ -0,0 +1,28 @@
+package mab.moneymanagement.view.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mab.moneymanagement.R;
+
+
+public class CategoryDetailFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_category_detail, container, false);
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+
+}
