5773d4585c979ab96fd973e3b8d0b545eca2627c
MahmoudBelal <mahmoudbelal955@gmail.com>
2018-03-14 11:48:38 +0200

    add class URL and Prefix in new package please call and follow this way

---
+++ app/src/main/java/mab/moneymanagement/util/URL.java
@@ -0,0 +1,20 @@
+package mab.moneymanagement.util;
+
+/**
+ * Created by user on 3/14/2018.
+ */
+
+public class URL {
+   /* ------- base url -------*/
+       public  static String  PATH = "http://gasem1234-001-site1.dtempurl.com";
+
+
+   /* ------ prefix url ----*/
+   public  static String  LOGIN = "/api/Login";
+   public  static String  REGISTER = "/api/Register";
+   public  static String RESET_PASSWORD = "/api/ResetPassword";
+
+
+
+
+}
